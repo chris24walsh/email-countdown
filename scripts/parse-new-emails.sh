f451c88b467c1ff5aa94e589631001eedb486396
root <root@mail.countdown.cf>
2017-12-16 17:59:31 +0000

    Fixed run-jobs
    Fixed few things with parse-new-emails also

--- scripts/parse-new-emails.sh
+++ scripts/parse-new-emails.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
-for i in /home/chris/mail/new/*
+for i in /home/email/mail/new/*
 do
 	if [ -f "$i" ]
 	then
 		email=`awk -F'<|>' '/^From:/{print $2}' "$i"`
 		message=`awk -F': ' '/^Subject:/{print $2}' "$i"`
 		startdate=`awk '/^Date:/{print $3 $4 $5}' "$i"`
-		enddate=`awk -F'i |@' '/^To:/{print $2}' "$i"`
-		jobfile="/home/chris/mail/jobs/job`date +%s`.txt"
+		enddate=`awk -F' |@' '/^To:/{print $2}' "$i"`
+		jobfile="/home/email/mail/jobs/job`date +%s.%N`.txt"
 		echo "Email: $email" > "$jobfile"
 		echo "Message: $message" >> "$jobfile"
 		echo "Start Date: $startdate" >> "$jobfile"
 		echo "End Date: $enddate" >> "$jobfile"
+		rm -f "$i"
+		/sbin/sendmail "$email" << EOF
+Subject: Reminder set: $message
+Thanks for using email-countdown!
+EOF
 	fi
 done
--- scripts/run-jobs.sh
+++ scripts/run-jobs.sh
@@ -1,12 +1,33 @@
 #!/bin/sh
 
-
-for i in *
+for i in /home/email/mail/jobs/*
 do
-	if [ -f "$i" ] && [ "$i" != "run-jobs.sh" ]
+	if [ -f "$i" ]
 	then
 		#parse first job
+
+		email=`awk -F': ' '/^Email:/{print $2}' "$i"`
+                message=`awk -F': ' '/^Message:/{print $2}' "$i"`
+                startdate=`awk -F': ' '/^Start Date:/{print $2}' "$i"`
+                enddate=`awk -F': ' '/^End Date:/{print $2}' "$i"`
+		
 		#send email with info
+		
+		sd=`date -d $startdate +%s`
+		ed=`date -d $enddate +%s`
+		diffSeconds=$(($ed-$sd))
+		diffDays=$(($diffSeconds/86400))
+
+		/sbin/sendmail "$email" << EOF
+Subject: $message
+You have $diffDays days left
+EOF
+		
 		#if last day of countdown, delete job
+
+		if [ "$startdate" == "$enddate" ]
+		then
+			rm -f "$i"
+		fi
 	fi
 done
