4993a8ed2db5187968b1cb2be6ab0c3c59f20602
root <root@mail.countdown.cf>
2017-12-17 18:01:47 +0000

    Add input sanitising to script that parses email

--- scripts/parse-new-emails.sh
+++ scripts/parse-new-emails.sh
@@ -5,9 +5,13 @@ do
 	if [ -f "$i" ]
 	then
 		email=`awk -F'<|>' '/^From:/{print $2}' "$i"`
+		email=${email//[^a-zA-Z0-9_-@.]/}
 		message=`awk -F': ' '/^Subject:/{print $2}' "$i"`
+		message=${message//[^a-zA-Z0-9 ,.?]/}
 		startdate=`awk '/^Date:/{print $3 $4 $5}' "$i"`
+		startdate=${startdate//[^a-zA-Z0-9]/}
 		enddate=`awk -F' |@' '/^To:/{print $2}' "$i"`
+		enddate=${enddate//[^a-zA-Z0-9]/}
 		jobfile="/home/email/mail/jobs/job`date +%s.%N`.txt"
 		echo "Email: $email" > "$jobfile"
 		echo "Message: $message" >> "$jobfile"
